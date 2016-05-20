9e63a7be9174755747027265e5e326b5421699db
msk30 <m3k1214@gmail.com>
2016-05-20 17:17:11 +0900

    セッション 修正 0520

--- session.php
+++ session.php
@@ -1,12 +1,16 @@
 <?php
 //ログインチェックとユーザ情報の保持
-
-
 //セッションの開始
 session_start();
 
+$u_name = $_SESSION["name"];
+$type_id = $_SESSION["type_id"];
+$aaa = $_SESSION['authenticated'];
+
 //ログイン認証済みでなければログインページへ移動
-if ($_SESSION["authenticated"] != TRUE) {
-  header("Location: ../view/acount/p_login.php");
-  exit;
+if ($aaa != TRUE) {
+	exit;
+
+  header("Location: acount/p_login.php");
+
 }
