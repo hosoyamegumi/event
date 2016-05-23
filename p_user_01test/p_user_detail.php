fe5944b4ea487caf896c376bfc3ff6e2bfe572e6
hosoyamegumi <hosoya1420371@gmail.com>
2016-05-23 16:07:31 +0900

    ユーザ詳細 0523

--- p_user_01test/p_user_detail.php
+++ p_user_01test/p_user_detail.php
@@ -1,7 +1,7 @@
 <?php
-// require_once "../session.php";
-// require_once "../util.inc.php";
-// require_once "../db.inc.php";
+ require_once "../session.php";
+ require_once "../util.inc.php";
+ require_once "../db.inc.php";
 ?>
 
 
