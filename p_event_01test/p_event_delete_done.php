66fe0491f5d0d55988908054921f80536890ef7d
inoueyumi <inoueyu.1110@gmail.com>
2016-05-23 10:27:51 +0900

    イベント情報削除の完了

---
+++ p_event_01test/p_event_delete_done.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8" />
+<title>イベント登録｜EventManager</title>
+<link href="../../css/reset.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../css/layout.css" />
+</head>
+<body>
+<div id="containner">
+	<?php include "../header.php"; ?>
+	<div id="main">
+		<!-- ページネーション挿入 -->
+	<h1>イベント削除</h1>
+	<p>イベントの削除が完了しました。</p>
+	<p><a href="p_event_list.php">イベント一覧に戻る</a></p>
+</div>
+</div>
+</body>
+</html>
