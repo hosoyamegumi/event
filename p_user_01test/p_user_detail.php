04d00140efce5957c1911942d4ee7efd7811fb86
hosoyamegumi <hosoya1420371@gmail.com>
2016-05-23 15:21:02 +0900

    ユーザ詳細 ０５２３

---
+++ p_user_01test/p_user_detail.php
@@ -0,0 +1,56 @@
+<?php
+// require_once "../session.php";
+// require_once "../util.inc.php";
+// require_once "../db.inc.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<!-- head -->
+  <head>
+	<meta charset="utf-8">
+	<title>ユーザー詳細｜EventManager</title>
+	<link href="../../css/reset.css" rel="stylesheet">
+	<link href="../../css/layout.css" rel="stylesheet">
+  </head>
+
+<!-- body -->
+  <body>
+	<div id="containner">
+
+<!-- header_include -->
+<?php //include "../header.php"; ?>
+
+<!-- main -->
+	  <div id="main">
+	    <h1>ユーザ詳細</h1>
+
+	<!-- page -->
+		<div id="page">
+		  <p>ページの表示</p>
+		  <?php //echo $this->pagination->create_links(); ?>
+		</div>
+
+	<!-- dl -->
+		<dl>
+		  <dt>ID</dt>
+		  <dd><?php echo h($u_id); ?></dd>
+		  <dt>氏名</dt>
+		  <dd><?php echo h($u_mane); ?></dd>
+		  <dt>所属グループ</dt>
+		  <dd><?php echo h($group_id); ?></dd>
+		</dl>
+
+	<!-- button -->
+		<p>
+		  <a href="p_user_list.php"><input type="submit" name="cancel" value="一覧に戻る" id="button_02"></a>
+		  <a href="p_user_edit.php"><input type="submit" name="edit" value="編集" id="button_04"></a>
+		  <a href="p_user_delete.php"><input type="submit" name="delete" value="削除" id="button_03"></a>
+		</p>
+
+	  </div>
+	</div>
+  </body>
+</html>
