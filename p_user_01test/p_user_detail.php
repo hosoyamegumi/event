bbb047f858e43ab50632a63b85b1d7c241056f4d
hosoyamegumi <hosoya1420371@gmail.com>
2016-05-24 16:39:52 +0900

    ユーザ詳細 モーダル修正済 0524
    モーダルの修正済み

--- p_user_01test/p_user_detail.php
+++ p_user_01test/p_user_detail.php
@@ -1,7 +1,7 @@
 <?php
- require_once "../session.php";
- require_once "../util.inc.php";
- require_once "../db.inc.php";
+// require_once "../session.php";
+// require_once "../util.inc.php";
+// require_once "../db.inc.php";
 ?>
 
 
@@ -12,8 +12,16 @@
   <head>
 	<meta charset="utf-8">
 	<title>ユーザー詳細｜EventManager</title>
+
+  <!-- css -->
 	<link href="../../css/reset.css" rel="stylesheet">
-	<link href="../../css/layout.css" rel="stylesheet">
+	<link href="../../css/remodal.css" rel="stylesheet">
+    <link href="../../css/remodal-default-theme.css" rel="stylesheet">
+    <link href="../../css/layout.css" rel="stylesheet">
+
+  <!-- js -->
+	<script src="../../js/jquery-loader.js"></script>
+	<script src="../../js/remodal.js"></script>
   </head>
 
 <!-- body -->
@@ -26,6 +34,12 @@
 <!-- main -->
 	  <div id="main">
 	    <h1>ユーザ詳細</h1>
+
+	<!-- page -->
+		<div id="page">
+		  <p>ページの表示</p>
+		  <?php //echo $this->pagination->create_links(); ?>
+		</div>
 
 	<!-- dl -->
 		<dl>
@@ -41,10 +55,24 @@
 		<p>
 		  <input type="submit" name="cancel" value="一覧に戻る" id="button_02">
 		  <input type="submit" name="edit" value="編集" id="button_04">
-		  <input type="submit" name="delete" value="削除" id="button_03">
+		  <input type="submit" name="delete" value="削除" id="button_03" data-remodal-target="modal">
 		</p>
 
 	  </div>
+	</div>
+
+<!-- modal -->
+	<div class="remodal" data-remodal-id="modal">
+      <p class="comment">本当に削除してよろしいですか？</p>
+
+	<!-- close_button -->
+      <a data-remodal-action="close" class="remodal-close"></a>
+
+	<!-- button -->
+	  <form method="post">
+		<input type="submit" name="cancel" value="Cancel" id="buttom_03">
+		<input type="submit" name="ok" value="OK" id="button_01">
+	  </form>
 	</div>
   </body>
 </html>
