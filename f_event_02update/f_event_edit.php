ed13496fe716403dbbcbc7890ebd4c1b28aa83b2
inoueyumi <inoueyu.1110@gmail.com>
2016-05-25 15:17:37 +0900

    イベント情報編集 インクルード OK 5/25

---
+++ f_event_02update/f_event_edit.php
@@ -0,0 +1,87 @@
+<?php
+require_once '../session.php';
+require_once '../util.inc.php';
+require_once '../db.inc.php';
+//ここでidの取得を行う
+ $id=$_GET["id"];
+//とりあえずベタ打ちでid
+// $id=1;
+try {
+	//--------------------
+	// データベースの準備
+	//--------------------
+	$pdo = db_init();
+	//--------------------
+	// 在庫リストの取得
+	//--------------------
+	$sql = "SELECT * FROM events WHERE id = ".$id;
+	$stmt = $pdo->query($sql);
+	$events = $stmt->fetch();
+	var_dump($events);
+	if($_SERVER["REQUEST_METHOD"]=="POST"){
+		//「更新」が押されたとき
+		$isValidated=TRUE;
+		$title=$_POST["title"];
+		$start=$_POST["start"];
+		$end=$_POST["end"];
+		$place=$_POST["place"];
+		$group_id=$_POST["group_id"];
+		$detail=$_POST["detail"];
+
+		//タイトルのバリデーション
+		if($title === ""){
+			$titleError="※タイトルを入力して下さい";
+			$isValidated=FALSE;
+		}
+
+	 	//開催日時のバリデーション
+ 		if($start === ""){
+ 			$startError="※開催日時を入力して下さい";
+ 			$isValidated=FALSE;
+ 		}
+		elseif(!preg_match("/^\d{4}-\d{2}-\d{2}[\s　]\d{2}:\d{2}:\d{2}$/", $start)){
+ 			$startError="日付は「0000-00-00 00:00:00」の形式で入力してください。";
+ 			$isValidated=FALSE;
+ 		}
+
+
+		//終了日時のバリデーション
+		if(!preg_match("/^\d{4}-\d{2}-\d{2}[\s　]\d{2}:\d{2}:\d{2}$/", $end)){
+			$endError="日付は「0000-00-00 00:00:00」の形式で入力してください。";
+			$isValidated=FALSE;
+		}
+
+		//場所のバリデーション
+		if($place === ""){
+			$placeError="※場所を入力して下さい";
+			$isValidated=FALSE;
+		}
+
+		if($isValidated==TRUE){
+			if(isset($_POST["save"])){
+				$dbobj=$pdo->prepare
+				("UPDATE events SET
+						title=?, start=?, end=?, place=?, group_id=?, detail=?
+						WHERE id=?");
+				$dbobj->execute(array($title,$start,$end,$place,$group_id,$detail,$id));
+				//編集後に移動するためのヘッダー
+ 				header("Location:p_event_edit_done.php");
+				exit;
+			}
+
+			//「キャンセル」が押されたとき
+			if(isset($_POST["cancel"])){
+ 				header("Location:p_event_detail.php");
+ 				exit;
+			}
+			$pdo = null;
+		}
+	}else{
+		$isValidated=FALSE;
+	}
+}
+catch (PDOException $e) {
+	echo $e->getMessage();
+	exit;
+}
+?>
