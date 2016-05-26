b6bf35de20142b729e4ec31915c708eb125ebe9f
inoueyumi <inoueyu.1110@gmail.com>
2016-05-26 14:13:46 +0900

    確認

--- f_user_01test/f_user_detail.php
+++ f_user_01test/f_user_detail.php
@@ -2,18 +2,13 @@
 //require_once "../session.php";
 require_once "../util.inc.php";
 require_once "../db.inc.php";
-
 $u_id=$_GET['id'];
-
-//if($_SESSION["flag"]=TRUE){
-	//header("Location:p_user_list");
-	//exit;
-	//}
-
+if(isset($_SESSION["flag"])){
+	header("Location:p_user_list");
+	exit;
+	}
 $pdo=db_init();
-
 //var_dump($pdo);
-
 $stmt=$pdo->prepare("
 								SELECT users.id AS u_id,
 											users.name AS u_name,
@@ -24,47 +19,33 @@ $stmt=$pdo->prepare("
 									WHERE users.id=?");
 $stmt->execute(array($u_id));
 $users=$stmt->fetch();
-
-var_dump($users);
-
+//var_dump($users);
 $userId=$users['u_id'];
 $userName=$users['u_name'];
 $g_name=$users['g_name'];
-
-
-
-
 if(isset($_POST['cancel'])){
 	header("Location:p_user_list.php");
 }
 if (isset($_POST['edit'])){
-	header("Location:user_edit.php");
+	header("Location:p_user_edit.php/?id={$u_id}");
 }
 if(isset($_POST['no'])){
 	header("Refresh:0; url=p_user_detail.php");
 }
-
-
 if (isset($_POST['ok'])){
 	try {
 		// データベースから削除
 		$sql = "DELETE  FROM users
 					  WHERE id=?";
-
 		$stmt = $pdo->prepare($sql);
-		$stmt->execute(array($id));
+		$stmt->execute(array($u_id));
 		// 完了画面へ移動
 		$_SESSION['flag']=TRUE;
-		header("Refresh:0; url=user_delete_done.php");
+		header("Refresh:0; url=p_user_delete_done.php");
 	}
 	catch (PDOException $e) {
 		echo $e->getMessage();
 		exit;
-
-
-
 }
-
 }
-
 ?>
