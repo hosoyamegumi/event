56febfc751dc4569aa046f314e0e2f13a4922e51
hosoyamegumi <hosoya1420371@gmail.com>
2016-05-26 17:21:40 +0900

    css確認済みhtml

---
+++ a_event/view/event/p_event_delete_done.php
@@ -0,0 +1,42 @@
+<!-- php -->
+<?php
+// 	require_once '../session.php';
+// 	require_once '../util.inc.php';
+?>
+
+<!-- html -->
+<!DOCTYPE html>
+<html lang="ja">
+
+<!-- head -->
+  <head>
+	<meta charset="utf-8" />
+	<title>イベント削除｜EventManager</title>
+
+<!-- css -->
+	<link href="../../css/reset.css" rel="stylesheet">
+	<link href="../../css/layout.css" rel="stylesheet">
+
+  </head>
+
+<!-- body -->
+  <body>
+	<div id="containner">
+
+<!-- header_include -->
+<?php include "../header.php"; ?>
+
+<!-- main -->
+	  <div id="main">
+		<h1>イベント削除</h1>
+
+	<!-- done -->
+		<div id="done">
+		  <p>イベントの削除が完了しました。</p>
+		  <p><a href="p_event_list.php">イベント一覧に戻る</a></p>
+		</div>
+
+	  </div>
+	</div>
+  </body>
+</html>
---
+++ a_event/view/event/p_event_edit_done.php
@@ -0,0 +1,43 @@
+<!-- php -->
+<?php
+// 	require_once '../session.php';
+// 	require_once '../util.inc.php';
+// 	$id=$_GET["id"];
+?>
+
+<!-- html -->
+<!DOCTYPE html>
+<html lang="ja">
+
+<!-- head -->
+  <head>
+	<meta charset="utf-8" />
+	<title>イベント編集｜EventManager</title>
+
+<!-- css -->
+	<link href="../../css/reset.css" rel="stylesheet">
+	<link href="../../css/layout.css" rel="stylesheet">
+
+  </head>
+
+<!-- body -->
+  <body>
+	<div id="containner">
+
+<!-- header_include -->
+<?php include "../header.php"; ?>
+
+<!-- main -->
+	  <div id="main">
+		<h1>イベント編集</h1>
+
+	<!-- done -->
+		<div id="done">
+		  <p>イベントの編集が完了しました。</p>
+		  <p><a href="p_event_detail.php">イベント詳細に戻る</a></p>
+		</div>
+
+	  </div>
+	</div>
+  </body>
+</html>
