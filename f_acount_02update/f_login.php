d8670d8100543938d143f9cc9ad5912eaf713756
msk30 <m3k1214@gmail.com>
2016-05-27 16:39:04 +0900

    Update f_login.php

--- f_acount_02update/f_login.php
+++ f_acount_02update/f_login.php
@@ -1,22 +1,13 @@
 <?php
 session_start();
-
 require_once '../db.inc.php';
 require_once '../util.inc.php';
-
-
 $login_id="";
 $login_pass="";
-
 if($_SERVER['REQUEST_METHOD']==="POST"){
-
 	$login_id=$_POST['login_id'];
 	$login_pass=$_POST['login_pass'];
-
 	$isValidated=TRUE;
-
-
-
 	if($login_id===""){
 		$ErrorId="ログインIDを入力してください";
 		$isValidated=FALSE;
@@ -25,32 +16,39 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
 		$ErrorPass="パスワードを入力してください";
 		$isValidated=FALSE;
 	}
+
+	if (preg_match("/[\s　]/u", $login_id)){
+		$ErrorId="ログインIDを入力してください";
+		$isValidated=FALSE;
+	}
+	if (preg_match("/[\s　]/", $login_pass)){
+		$ErrorPass="パスワードを正しく入力してください";
+		$isValidated=FALSE;
+	}
+var_dump($_POST);
 	if($isValidated==TRUE){
 		try {
 			$pdo=db_init();
-
 			$sql= "SELECT * FROM users
 						WHERE
 							login_id=?
 						AND
 							login_pass=?";
-
 			$stmt=$pdo->prepare($sql);
 			$stmt->execute(array($login_id,sha1($login_pass."abc")));
 			$info=$stmt->fetch();
-
 			if ($info==FALSE){
 				$ErrorId="ログインIDまたはパスワードが間違っています";
 				$isValidated=FALSE;
 			}
+
+
 			if($info !=FALSE){
 				$_SESSION['authenticated']=TRUE;
 				$_SESSION["name"]=$info["name"];
 				$_SESSION["type_id"]=$info["type_id"];
 				$_SESSION["u_id"]=$info["id"];
-
 				var_dump($_SESSION);
-
 				header("Location:../event/p_event_today.php");
 				exit;
 			}
@@ -61,6 +59,4 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
 		}
 	}
 }
-
-
 ?>
