42d3795fee431e468a641dc3d4d40132746a2e35
inoueyumi <inoueyu.1110@gmail.com>
2016-05-27 10:36:56 +0900

    インクルード OK 5/27

--- f_user_02update/f_user_edit.php
+++ f_user_02update/f_user_edit.php
@@ -1,33 +1,52 @@
 <?php
   require_once "../util.inc.php";
   require_once "../db.inc.php";
-  //require_once '../session.php';
+  require_once '../session.php';
   $u_id=$_GET['id'];
   try{
   	//dbの接続
   	$pdo=db_init();
-$stmt=$pdo->prepare("
-								SELECT *
-								FROM users WHERE id=?");
-$stmt->execute(array($u_id));
-$users=$stmt->fetch();
-  //var_dump($users);
-$u_name=$users['name'];
-$login_id=$users['id'];
-$login_pass=$users['login_pass'];
-$group_id=$users['type_id'];
-if(isset($_POST['save'])){
+  	$stmt=$pdo->prepare("
+  			SELECT *
+  			FROM users WHERE id=?");
+  	$stmt->execute(array($u_id));
+  	$users=$stmt->fetch();
+
+  	$userName=$users['name'];
+  	$login_id=$users['id'];
+  	$login_pass=$users['login_pass'];
+  	$group_id=$users['type_id'];
+
+  	if(isset($_POST['save'])){
+
 	$isValidated=TRUE;
-	$u_name=$_POST['u_name'];
+	$userName=$_POST['userName'];
 	$login_id=$_POST['login_id'];
 	$login_pass=$_POST['login_pass'];
 	$group_id=$_POST['group_id'];
-	if($u_name===""){
-		$u_nameError="氏名を入力してください";
+var_dump($_POST);
+
+
+	if(preg_match("/[\s　]/u", $userName)){
+		$userNameError="氏名を入力してください";
 		$isValidated=FALSE;
 	}
-	if ($login_id===""){
+	if ($userName ==""){
+		$userNameError="氏名を入力してください";
+		$isValidated=FALSE;
+	}
+
+	if (preg_match("/[\s　]/", $login_id)){
 		$login_idError="IDを入力してください";
+		$isValidated=FALSE;
+	}
+	if ($login_id ==""){
+		$login_idError="IDを入力してください";
+		$isValidated=FALSE;
+	}
+
+	if (preg_match("/[\s　]/", $login_pass)){
+		$login_passError="パスワードを正しく入力してください";
 		$isValidated=FALSE;
 	}
 	if($isValidated==TRUE){
@@ -40,14 +59,14 @@ if(isset($_POST['save'])){
 			$stmt=$pdo->prepare($sql);
 			$stmt->execute(array(
 					$login_id,
-					$login_pass,
-					$u_name,
+					sha1($login_pass."abc"),
+					$userName,
 					$group_id,
 					$u_id)
 					);
-		}
-		header("Location:p_user_edit_done.php?id={$u_id}");
-		exit;
+			header("Location:p_user_edit_done.php?id={$u_id}");
+			exit;
+	}
 		}
 	}
 	catch (PDOException $e){
